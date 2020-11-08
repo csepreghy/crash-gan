2a0c5885089f9c189ba07f729f8c0c62f64d226d
Andrew <andris788@gmail.com>
2020-11-08 14:05:20 +0100

    (nn): fixed indentation, changed resolution

--- gan/gan.py
+++ gan/gan.py
@@ -21,28 +21,26 @@ from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Input, Dense, Reshape, Flatten
 from tensorflow.keras.layers import BatchNormalization, LeakyReLU
-from tensorflow.keras.layers import 
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Define input image dimensions
-#Large images take too much time and resources.
-img_rows = 28
-img_cols = 28
-channels = 1
+# Define input image dimensions
+# Large images take too much time and resources.
+img_rows = 360
+img_cols = 640
+channels = 3
 img_shape = (img_rows, img_cols, channels)
 
 ##########################################################################
-#Given input of noise (latent) vector, the Generator produces an image.
+# Given input of noise (latent) vector, the Generator produces an image.
 def build_generator():
+    noise_shape = (1024,) #1D array of size 100 (latent vector / noise)
 
-    noise_shape = (100,) #1D array of size 100 (latent vector / noise)
-
-#Define your generator network 
-#Here we are only using Dense layers. But network can be complicated based
-#on the application. For example, you can use VGG for super res. GAN.         
+# Define your generator network 
+# Here we are only using Dense layers. But network can be complicated based
+# on the application. For example, you can use VGG for super res. GAN.         
 
     model = Sequential()
 
@@ -66,13 +64,13 @@ def build_generator():
 
     return Model(noise, img)
 
-#Alpha — α is a hyperparameter which controls the underlying value to which the
-#function saturates negatives network inputs.
-#Momentum — Speed up the training
+# Alpha — α is a hyperparameter which controls the underlying value to which the
+# function saturates negatives network inputs.
+# Momentum — Speed up the training
 ##########################################################################
 
-#Given an input image, the Discriminator outputs the likelihood of the image being real.
-    #Binary classification - true or false (we're calling it validity)
+# Given an input image, the Discriminator outputs the likelihood of the image being real.
+# Binary classification - true or false (we're calling it validity)
 
 def build_discriminator():
 
@@ -91,12 +89,13 @@ def build_discriminator():
     validity = model(img)
 
     return Model(img, validity)
-#The validity is the Discriminator’s guess of input being real or not.
+
+# The validity is the Discriminator’s guess of input being real or not.
 
 
-#Now that we have constructed our two models it’s time to pit them against each other.
-#We do this by defining a training function, loading the data set, re-scaling our training
-#images and setting the ground truths. 
+# Now that we have constructed our two models it’s time to pit them against each other.
+# We do this by defining a training function, loading the data set, re-scaling our training
+# images and setting the ground truths. 
 def train(epochs, batch_size=128, save_interval=50):
 
     # Load the dataset
@@ -105,16 +104,16 @@ def train(epochs, batch_size=128, save_interval=50):
     # Convert to float and Rescale -1 to 1 (Can also do 0 to 1)
     X_train = (X_train.astype(np.float32) - 127.5) / 127.5
 
-#Add channels dimension. As the input to our gen and discr. has a shape 28x28x1.
+# Add channels dimension. As the input to our gen and discr. has a shape 28x28x1.
     X_train = np.expand_dims(X_train, axis=3) 
 
     half_batch = int(batch_size / 2)
 
 
-#We then loop through a number of epochs to train our Discriminator by first selecting
-#a random batch of images from our true dataset, generating a set of images from our
-#Generator, feeding both set of images into our Discriminator, and finally setting the
-#loss parameters for both the real and fake images, as well as the combined loss. 
+# We then loop through a number of epochs to train our Discriminator by first selecting
+# a random batch of images from our true dataset, generating a set of images from our
+# Generator, feeding both set of images into our Discriminator, and finally setting the
+# loss parameters for both the real and fake images, as well as the combined loss. 
     
     for epoch in range(epochs):
 
@@ -126,36 +125,38 @@ def train(epochs, batch_size=128, save_interval=50):
         idx = np.random.randint(0, X_train.shape[0], half_batch)
         imgs = X_train[idx]
 
- 
         noise = np.random.normal(0, 1, (half_batch, 100))
 
         # Generate a half batch of fake images
         gen_imgs = generator.predict(noise)
 
         # Train the discriminator on real and fake images, separately
-        #Research showed that separate training is more effective. 
+        # Research showed that separate training is more effective. 
         d_loss_real = discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
         d_loss_fake = discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
-    #take average loss from real and fake images. 
-    #
+        # take average loss from real and fake images. 
+
         d_loss = 0.5 * np.add(d_loss_real, d_loss_fake) 
 
-#And within the same loop we train our Generator, by setting the input noise and
-#ultimately training the Generator to have the Discriminator label its samples as valid
-#by specifying the gradient loss.
+# And within the same loop we train our Generator, by setting the input noise and
+# ultimately training the Generator to have the Discriminator label its samples as valid
+# by specifying the gradient loss.
+
         # ---------------------
         #  Train Generator
         # ---------------------
-#Create noise vectors as input for generator. 
-#Create as many noise vectors as defined by the batch size. 
-#Based on normal distribution. Output will be of size (batch size, 100)
+
+# Create noise vectors as input for generator. 
+# Create as many noise vectors as defined by the batch size. 
+# Based on normal distribution. Output will be of size (batch size, 100)
+
         noise = np.random.normal(0, 1, (batch_size, 100)) 
 
         # The generator wants the discriminator to label the generated samples
         # as valid (ones)
-        #This is where the genrator is trying to trick discriminator into believing
-        #the generated image is true (hence value of 1 for y)
-        valid_y = np.array([1] * batch_size) #Creates an array of all ones of size=batch size
+        # This is where the genrator is trying to trick discriminator into believing
+        # the generated image is true (hence value of 1 for y)
+        valid_y = np.array([1] * batch_size) # Creates an array of all ones of size=batch size
 
         # Generator is part of combined where it got directly linked with the discriminator
         # Train the generator with noise as x and 1 as y. 
@@ -164,18 +165,18 @@ def train(epochs, batch_size=128, save_interval=50):
         g_loss = combined.train_on_batch(noise, valid_y)
 
 
-#Additionally, in order for us to keep track of our training process, we print the
-#progress and save the sample image output depending on the epoch interval specified.  
+# Additionally, in order for us to keep track of our training process, we print the
+# progress and save the sample image output depending on the epoch interval specified.  
 # Plot the progress
         
-        print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+        print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
 
         # If at save interval => save generated image samples
         if epoch % save_interval == 0:
             save_imgs(epoch)
 
-#when the specific sample_interval is hit, we call the
-#sample_image function. Which looks as follows.
+# when the specific sample_interval is hit, we call the
+# sample_image function. Which looks as follows.
 
 def save_imgs(epoch):
     r, c = 5, 5
@@ -209,9 +210,7 @@ optimizer = Adam(0.0002, 0.5)  #Learning rate and momentum.
 #Binary cross entropy as we are doing prediction and it is a better
 #loss function compared to MSE or other. 
 discriminator = build_discriminator()
-discriminator.compile(loss='binary_crossentropy',
-    optimizer=optimizer,
-    metrics=['accuracy'])
+discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
 #build and compile our Discriminator, pick the loss function
 
@@ -224,9 +223,9 @@ generator.compile(loss='binary_crossentropy', optimizer=optimizer)
 z = Input(shape=(100,))   #Our random input to the generator
 img = generator(z)
 
-#This ensures that when we combine our networks we only train the Generator.
-#While generator training we do not want discriminator weights to be adjusted. 
-#This Doesn't affect the above descriminator training.     
+# This ensures that when we combine our networks we only train the Generator.
+# While generator training we do not want discriminator weights to be adjusted. 
+# This Doesn't affect the above descriminator training.     
 discriminator.trainable = False  
 
 #This specifies that our Discriminator will take the images generated by our Generator
@@ -245,7 +244,7 @@ combined = Model(z, valid)
 combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
 
-train(epochs=100, batch_size=32, save_interval=10)
+train(epochs=10000, batch_size=32, save_interval=100)
 
 #Save model for future use to generate fake images
 #Not tested yet... make sure right model is being saved..
