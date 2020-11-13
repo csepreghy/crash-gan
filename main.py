e24d6fc1a72175101409d4b46a5ab557804598c5
Andrew <andris788@gmail.com>
2020-11-13 10:29:35 +0100

    (nn): another shot at fixing imports

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# Standard library imports
 import numpy as np
 
-# Local Imports
-from gan import GAN
-from utils import load_frames
-import config
+# Local application imports
+from src.gan import GAN
+from src.utils import load_frames
+import src.config as config
 
 if __name__ == '__main__':
     imgs = load_frames(n_samples=20, source_folder=config.source_folder)
